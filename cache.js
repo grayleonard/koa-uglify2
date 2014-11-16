6ac303c869fd45041fb7a3c5566f3ad574369d0c
Gray Leonard <gl7039a@american.edu>
2014-11-16 14:55:42 -0500

    Improved caching - cached files include dirname

--- cache.js
+++ cache.js
@@ -8,16 +8,18 @@ var cache = module.exports;
 cache.is_cached = function(file_path) {
 	if(!fs.existsSync(cache_path))
 		fs.mkdirSync(cache_path, 0755);
-
-	var cached_file_path = path.normalize(cache_path + path.basename(file_path).replace('.js', '.ugly.js'));
+	var file_path_dir = path.dirname(file_path).replace(/\//g, '.').substr(1).concat('.');
+	console.log(file_path_dir);
+	var cached_file_path = path.normalize(cache_path + file_path_dir + path.basename(file_path).replace('.js', '.ugly.js'));
 	if(fs.existsSync(cached_file_path))
 		return true;
 	return false;
 }
 
 cache.cache_file = function(file_path, data) {
-	var tocache_path = path.normalize(cache_path+path.basename(file_path).replace(".js", ".ugly.js"));
-	fs.writeFile(path.normalize(tocache_path), data, "utf-8",
+	var file_path_dir = path.dirname(file_path).replace(/\//g, '.').substr(1).concat('.');
+	var tocache_path = path.normalize(cache_path + file_path_dir + path.basename(file_path).replace('.js', '.ugly.js'));
+ 	fs.writeFile(path.normalize(tocache_path), data, "utf-8",
 	function(err) {
 		if(err) console.log(err);
 	});
@@ -25,7 +27,8 @@ cache.cache_file = function(file_path, data) {
 	
 
 cache.fetch_file = function(file_path) {
-	var fetch_path = path.normalize(cache_path+path.basename(file_path).replace(".js", ".ugly.js"));
+	var file_path_dir = path.dirname(file_path).replace(/\//g, '.').substr(1).concat('.');
+	var fetch_path = path.normalize(cache_path + file_path_dir + path.basename(file_path).replace('.js', '.ugly.js'));
 	var cached_file = fs.readFileSync(fetch_path, 'utf8');
 	return cached_file;
 }
--- index.js
+++ index.js
@@ -11,37 +11,42 @@ module.exports = function(options) {
 	return function*(next) {
 		if(/(\.js)$/.test(this.path) != true) {
 			yield next;
+			return
+		}
+		var start = new Date;
+		var file_path = url.parse(this.url).pathname;
+		var file_exists = yield fs.exists(path.normalize(src + file_path));
+		if(!file_exists) {
+			yield next;
+			return;
+		}
+		if(cache.is_cached(file_path)) {
+			var type = path.extname(file_path).substr(1);
+			this.body = cache.fetch_file(file_path);
+			this.type = type;
+			var ms = new Date - start;
+			console.log('%s %s - %s', this.method, this.url, ms);
 		} else {
-			var start = new Date;
-			var file_path = url.parse(this.url).pathname;
-			if(cache.is_cached(file_path)) {
-				var type = path.extname(file_path).substr(1);
-				this.body = cache.fetch_file(file_path);
+			var file = yield fs.readFile(path.normalize(src + file_path), 'utf8');
+			var type = path.extname(file_path).substr(1);
+			var ast;
+			try {
+				ast = uglify.parse(file);
+				ast.figure_out_scope();
+				var compressor = uglify.Compressor();
+				ast = ast.transform(compressor);
+				ast.figure_out_scope();
+				ast.compute_char_frequency();
+				ast.mangle_names();
+			} catch (x) {
+				console.log(x);
+			}
+			if(ast) {
+				this.body = ast.print_to_string();
 				this.type = type;
+				cache.cache_file(file_path, ast.print_to_string());
 				var ms = new Date - start;
 				console.log('%s %s - %s', this.method, this.url, ms);
-			} else {
-				var file = yield fs.readFile(src + file_path, 'utf8');
-				var type = path.extname(file_path).substr(1);
-				var ast;
-				try {
-					ast = uglify.parse(file);
-					ast.figure_out_scope();
-					var compressor = uglify.Compressor();
-					ast = ast.transform(compressor);
-					ast.figure_out_scope();
-					ast.compute_char_frequency();
-					ast.mangle_names();
-				} catch (x) {
-					console.log(x);
-				}
-				if(ast) {
-					this.body = ast.print_to_string();
-					this.type = type;
-					cache.cache_file(src + file_path, ast.print_to_string());
-					var ms = new Date - start;
-					console.log('%s %s - %s', this.method, this.url, ms);
-				}
 			}
 		}
 	}
